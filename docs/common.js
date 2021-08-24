d7960130002d472696fabcea110ec5263601a358
soyeonlee57 <soyeonlee57@gmail.com>
2021-08-24 16:07:43 +0900

    portfolio modify

--- docs/common.js
+++ docs/common.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 
 
 	new fullpage('#fullpage', {
-		anchors: ['section1', 'section2', 'section3', 'section4','section5'],
+		anchors: ['section1', 'section2', 'section3', 'section4','section5','section6'],
 	});
 	/*
 	This pen cleverly utilizes SVG filters to create a "Morphing Text" effect. Essentially, it layers 2 text elements on top of each other, and blurs them depending on which text element should be more visible. Once the blurring is applied, both texts are fed through a threshold filter together, which produces the "gooey" effect. Check the CSS - Comment the #container rule's filter out to see how the blurring works!
